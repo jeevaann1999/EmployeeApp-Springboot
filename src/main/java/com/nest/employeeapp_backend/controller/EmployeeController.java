00194dae2b46d599f973d7005c0e7cf72045d949
jeevaann1999 <jeevaann1999@gmail.com>
2022-12-05 17:13:14 +0530

    post method

--- src/main/java/com/nest/employeeapp_backend/controller/EmployeeController.java
+++ src/main/java/com/nest/employeeapp_backend/controller/EmployeeController.java
@@ -1,23 +1,24 @@
 package com.nest.employeeapp_backend.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class EmployeeController {
-    @GetMapping("/")
+    @PostMapping("/")
     public String EmployeeWelcomepage(){
         return  "Welcome to the website";
     }
-    @GetMapping("/add")
+    @PostMapping("/add")
     public  String EmployeeAddpage(){
         return "Welcome to the add page";
     }
-    @GetMapping("/search")
+    @PostMapping("/search")
     public  String EmployeeSearchpage(){
         return  "Welcome to the search page";
     }
-    @GetMapping("/edit")
+    @PostMapping("/edit")
     public  String EmployeeEditpage(){
         return  "Welcome to the edit page";
     }
@@ -25,7 +26,7 @@ public class EmployeeController {
     public String ViewAllPage(){
         return  "Welcome to the view all page";
     }
-    @GetMapping("/delete")
+    @PostMapping("/delete")
     public String EmployeeDeletepage(){
         return "Welcome to the delete page";
     }
