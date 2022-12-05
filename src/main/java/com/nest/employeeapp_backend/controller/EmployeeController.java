0c65626cb52f7b144f91183b3e99d03739d2313d
jeevaann1999 <jeevaann1999@gmail.com>
2022-12-05 15:32:16 +0530

    first commit

---
+++ src/main/java/com/nest/employeeapp_backend/controller/EmployeeController.java
@@ -0,0 +1,7 @@
+package com.nest.employeeapp_backend.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class EmployeeController {
+}
---
+++ src/test/java/com/nest/employeeapp_backend/EmployeeappBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nest.employeeapp_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeappBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
