9e376773fc0becf6eceafb82c0afac3bfde94e55
jeevaann1999 <jeevaann1999@gmail.com>
2022-12-05 16:32:39 +0530

    bugfix

--- src/main/java/com/nest/employeeapp_backend/controller/EmployeeController.java
+++ src/main/java/com/nest/employeeapp_backend/controller/EmployeeController.java
@@ -6,19 +6,19 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class EmployeeController {
     @GetMapping("/")
-    public String Welcomepage(){
+    public String EmployeeWelcomepage(){
         return  "Welcome to the website";
     }
     @GetMapping("/add")
-    public  String Addpage(){
+    public  String EmployeeAddpage(){
         return "Welcome to the add page";
     }
     @GetMapping("/search")
-    public  String Searchpage(){
+    public  String EmployeeSearchpage(){
         return  "Welcome to the search page";
     }
     @GetMapping("/edit")
-    public  String Editpage(){
+    public  String EmployeeEditpage(){
         return  "Welcome to the edit page";
     }
     @GetMapping("/view")
@@ -26,7 +26,7 @@ public class EmployeeController {
         return  "Welcome to the view all page";
     }
     @GetMapping("/delete")
-    public String Deletepage(){
+    public String EmployeeDeletepage(){
         return "Welcome to the delete page";
     }
 }
