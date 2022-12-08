af3b2ac56fe8cbe994a4af3d5aa769308144d487
jeevaann1999 <jeevaann1999@gmail.com>
2022-12-08 12:21:53 +0530

    delete

--- src/main/java/com/nest/employeeapp_backend/controller/EmployeeController.java
+++ src/main/java/com/nest/employeeapp_backend/controller/EmployeeController.java
@@ -5,19 +5,23 @@ import com.nest.employeeapp_backend.model.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 public class EmployeeController {
     @Autowired
     private EmployeeDao dao;
+
     @PostMapping("/")
-    public String EmployeeWelcomepage(){
-        return  "Welcome to the website";
+    public String EmployeeWelcomepage() {
+        return "Welcome to the website";
     }
+
     @CrossOrigin(origins = "*")
-    @PostMapping(path = "/add",consumes = "application/json",produces="application/json")
-    public  String EmployeeAddpage(@RequestBody Employee e){
+    @PostMapping(path = "/add", consumes = "application/json", produces = "application/json")
+    public String EmployeeAddpage(@RequestBody Employee e) {
         System.out.println(e.getEmpcode());
         System.out.println(e.getName().toString());
         System.out.println(e.getDesignation().toString());
@@ -29,26 +33,35 @@ public class EmployeeController {
         dao.save(e);
         return "Employee Added Successfully";
     }
+
     @CrossOrigin(origins = "*")
     @PostMapping("/search")
-    public  List<Employee> EmployeeSearchpage(@RequestBody Employee e){
-        String empcode=String.valueOf(e.getEmpcode());
+    public List<Employee> EmployeeSearchpage(@RequestBody Employee e) {
+        String empcode = String.valueOf(e.getEmpcode());
         System.out.println(empcode);
         return (List<Employee>) dao.SearchEmployee(e.getEmpcode());
 
     }
+
     @PostMapping("/edit")
-    public  String EmployeeEditpage(){
-        return  "Welcome to the edit page";
+    public String EmployeeEditpage() {
+        return "Welcome to the edit page";
     }
 
     @CrossOrigin(origins = "*")
     @GetMapping("/view")
-    public List<Employee> ViewAllPage(){
+    public List<Employee> ViewAllPage() {
         return (List<Employee>) dao.findAll();
     }
+
+    @CrossOrigin(origins = "*")
     @PostMapping("/delete")
-    public String EmployeeDeletepage(){
-        return "Welcome to the delete page";
+    public HashMap<String, String> EmployeeDeletepage(@RequestBody Employee e) {
+        String empid = String.valueOf((e.getId()));
+        System.out.println(empid);
+        dao.deleteEmployee(e.getId());
+        HashMap<String, String> map = new HashMap<>();
+        map.put("status", "success");
+        return map;
     }
 }
--- src/main/java/com/nest/employeeapp_backend/dao/EmployeeDao.java
+++ src/main/java/com/nest/employeeapp_backend/dao/EmployeeDao.java
@@ -1,6 +1,8 @@
 package com.nest.employeeapp_backend.dao;
 
 import com.nest.employeeapp_backend.model.Employee;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -9,6 +11,11 @@ import java.util.List;
 
 public interface EmployeeDao extends CrudRepository<Employee,Integer> {
 
-    @Query(value = "SELECT `id`, `company_name`, `designation`, `empcode`, `mobno`, `name`, `password`, `salary`, `username` FROM `employee` WHERE `empcode`= :empcode", nativeQuery = true)
+    @Query(value = "SELECT `id`, `company_name`, `designation`, `empcode`, `mobno`, `name`, `password`, `salary`, `username` FROM `employee` WHERE `empcode` LIKE %:empcode%", nativeQuery = true)
     List<Employee> SearchEmployee(@Param("empcode") Integer empcode);
+
+    @Modifying
+    @Transactional
+    @Query(value = "DELETE FROM `employee` WHERE `id`=:id",nativeQuery = true)
+    void deleteEmployee(@Param("id") Integer id);
 }
